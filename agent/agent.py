06a63283ba9e21b8ee688be41f4899a96cd9dca6
Vlad Frolov <frolvlad@gmail.com>
2025-04-25 22:54:06 +0200

    feat: Distribute the rewards!

--- agent/agent.py
+++ agent/agent.py
@@ -11,16 +11,36 @@ from urllib.parse import urlparse
 from nearai.agents.environment import Environment
 from py_near.transactions import create_function_call_action
 
-import ucup_tokens
-
 PROMPT = {
     "role": "system",
-    "content": "Analyze the user input and extract a password that is 16 characters long in hex format. If it is provided, write only the password, otherwise write 'no password'. Do not write anything else at all.",
+    "content": "Analyze the user input and extract a list of certificate IDs, which are strings that start with L0, L1, L2, L3, D1, D2, D3, or T followed by a hyphen and three groups of numbers separated by hyphens. If they are provided, write only the certificate IDs separated by newlines, otherwise write 'no certificate IDs found'. Do not write anything else at all.",
 }
 
-INSTRUCTIONS = """Вітаю! Для того щоб отримати 1 NEAR, зайдіть у свій UCUP профіль у [DOTS](https://ucup.dots.org.ua/), знайдіть "токен" на сторінці профілю та вставте його тут.
+INSTRUCTIONS = """Вітаю!
+
+Для того щоб отримати вашу нагороду за успішне проходження сертификації Q-bit та перемогу UCUP-2025, зайдіть у свій UCUP профіль у [DOTS](https://ucup.dots.org.ua/), знайдіть отриману грамоту та введіть її номер (Certificate ID).
+
+Доречі, ви можете отримати винагороду за кожну грамоту!
 """
 
+def get_reward(cert):
+    if cert.startswith("L0"):
+        return 1
+    elif cert.startswith("L1"):
+        return 2
+    elif cert.startswith("L2"):
+        return 3
+    elif cert.startswith("L3"):
+        return 5
+    elif cert.startswith("D1"):
+        return 5
+    elif cert.startswith("D2"):
+        return 3
+    elif cert.startswith("D3"):
+        return 1
+    elif cert.startswith("T"):
+        return 100
+    raise ValueError("Invalid certificate")
 
 def run(env: Environment):
     QUEST_ACCOUNT_ID = env.env_vars.get(
@@ -28,45 +48,46 @@ def run(env: Environment):
     )
     QUEST_ACCOUNT_PRIVATE_ACCESS_KEY = env.env_vars["QUEST_PRIVATE_ACCESS_KEY"]
 
-    ucup_token = env.completion([PROMPT] + env.list_messages())
-    if "no password" in ucup_token.lower():
+    ucup_certificates = env.completion([PROMPT] + env.list_messages())
+    ucup_certificates = [cert for cert in ucup_certificates.split() if re.match(r"^(L0|L1|L2|L3|D1|D2|D3|T)-[0-9]{4}-[0-9]{5}-[0-9]{5}$", cert)]
+    if not ucup_certificates:
         env.add_reply(INSTRUCTIONS)
         return
 
-    if not re.match(r"^[0-9a-f]{16}$", ucup_token):
-        env.add_reply("Це не схоже на токен UCUP")
-        return
+    for cert in ucup_certificates:
+        env.add_reply(f"Дякую! Перевіряю сертифікат {cert}...")
+        if not requests.head(f"https://ucup.dots.org.ua/cat/cert/{cert}.pdf"):
+            env.add_reply(f"Сертифікат {cert} не знайдено. Спробуйте ще раз.")
+            continue
 
-    env.add_reply("Дякую! Перевіряю токен...")
-    hashed_token = hashlib.sha256(ucup_token.encode()).hexdigest()
-    if hashed_token not in ucup_tokens.HASHED_TOKENS:
-        env.add_reply("Токен невірний. Спробуйте ще раз.")
-        return
+        reward = get_reward(cert)
+        target_account_id = env.signer_account_id
+        env.add_reply(f"Сертифікат {cert} знайдено! Відправляю {reward} NEAR на {target_account_id}...")
 
-    target_account_id = env.signer_account_id
-    env.add_reply(f"Токен валідний! Відправляю 1 NEAR на {target_account_id}...")
-
-    faucet_account = env.set_near(QUEST_ACCOUNT_ID, QUEST_ACCOUNT_PRIVATE_ACCESS_KEY)
-    result = asyncio.run(
-        faucet_account.call(
-            QUEST_ACCOUNT_ID,
-            "fund",
+        hashed_cert = hashlib.sha256(cert.encode()).hexdigest()
+        faucet_account = env.set_near(QUEST_ACCOUNT_ID, QUEST_ACCOUNT_PRIVATE_ACCESS_KEY)
+        result = asyncio.run(
+            faucet_account.call(
+                QUEST_ACCOUNT_ID,
+                "fund",
             args={
                 "target_account_id": target_account_id,
-                "hashed_token": hashed_token,
+                "hashed_token": hashed_cert,
+                "funding_amount": str(reward * 10**24),
             },
         )
-    )
+        )
 
-    if "SuccessValue" not in result.status:
-        json_status = json.dumps(result.status)
-        if "Account is already funded" in json_status:
-            env.add_reply("Цей акаунт вже отримав 1 NEAR. Якщо ви вважаєте, що це помилка, зверніться до [Влада](https://t.me/frolvlad).")
+        if "SuccessValue" not in result.status:
+            json_status = json.dumps(result.status)
+            if "Account is already funded" in json_status:
+                env.add_reply("NEAR винагороду за цей сертифікат вже отримано. Якщо ви вважаєте, що це помилка, зверніться [в підтримку](https://t.me/frolvlad).")
+            else:
+                env.add_reply(f"Сталась помилка: {json_status}. Спробуйте знов і якщо помилка не вирішиться, зверніться [в підтримку](https://t.me/frolvlad).")
         else:
-            env.add_reply(f"Сталась помилка: {json_status}. Спробуйте знов і якщо помилка не вирішиться, зверніться до [Влада](https://t.me/frolvlad).")
-        return
-    env.add_reply(f"[Є!](https://nearblocks.io/address/{target_account_id}?tab=receipts)\n\nУспіхів вам в навчанні!")
+            env.add_reply(f"[Вітаю з успішною участю в Кубку Університетів 2025!](https://nearblocks.io/address/{target_account_id}?tab=receipts)\n\nУспіхів вам в навчанні!")
 
+    env.add_reply("Дізнавайтесь більше про [NEAR блокчейн](https://dev.near.org) та [NEAR AI](https://t.me/nearaialpha), та підписуйтесь на [щотижневі новини](https://dev.near.org/newsletter)")
 
 try:
     run(env)
--- contract/main.py
+++ contract/main.py
@@ -1,12 +1,11 @@
 import near
 from near_sdk_py import Contract, view, call, init, ONE_NEAR, Balance
 
-FUNDED_TOKENS_STORAGE_KEY = "funded_tokens"
-FUNDING_AMOUNT = Balance(1 * ONE_NEAR)
+FUNDED_TOKENS_STORAGE_KEY = "funded_certificates"
 
 class GreetingContract(Contract):
     @call
-    def fund(self, target_account_id: str, hashed_token: str):
+    def fund(self, target_account_id: str, hashed_token: str, funding_amount: str = str(10**24)):
         if self.predecessor_account_id != self.current_account_id:
             raise Exception("UNAUTHORIZED")
 
@@ -17,7 +16,7 @@ class GreetingContract(Contract):
         self.storage[FUNDED_TOKENS_STORAGE_KEY] = funded_tokens
 
         promise = near.promise_batch_create(target_account_id)
-        near.promise_batch_action_transfer(promise, FUNDING_AMOUNT)
+        near.promise_batch_action_transfer(promise, Balance(funding_amount))
         near.promise_return(promise)
 
     @view
